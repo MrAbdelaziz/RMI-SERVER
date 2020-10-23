bb95d4bc45f89183d634c493493577faad2429a3
MrAbdelaziz <elouahab.abdelaziz@gmail.com>
2020-10-23 17:48:25 +0100

    update

--- src/server/Server.java
+++ src/server/Server.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package server3;
+package server;
 
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
@@ -13,7 +13,7 @@ import Impl.*;
  *
  * @author a
  */
-public class Server3 {
+public class Server {
 
     /**
      * @param args the command line arguments
