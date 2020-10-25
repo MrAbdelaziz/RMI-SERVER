21e0067893ea8c23d82381263a387d45c9263a15
MrAbdelaziz <elouahab.abdelaziz@gmail.com>
2020-10-25 20:50:15 +0100

    RMI+JNDI

--- src/server/Server.java
+++ src/server/Server.java
@@ -5,8 +5,12 @@
  */
 package server;
 
-import java.rmi.Naming;
+
 import java.rmi.registry.LocateRegistry;
+import java.util.Hashtable;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
 import Impl.*;
 
 /**
@@ -20,7 +24,7 @@ public class Server {
      */
     public static void main(String[] args) {
         try {
-        	
+   
             System.out.println("Serveur : Construction .. ");
             
             Reverse rev = new Reverse();
@@ -30,14 +34,24 @@ public class Server {
             
             System.out.println("Objet Reverse liee dans le RMIregistry");
             LocateRegistry.createRegistry(1099);
+            /* RMI  
+		            //Naming
+		            Naming.rebind("rmi://localhost/rev", rev);
+		            Naming.rebind("rmi://localhost/trie", tl);
+		            Naming.rebind("rmi://localhost/produit", pr);
+		            Naming.rebind("rmi://localhost/palindrome", pl); 
+            */
             
-            //Naming
-            Naming.rebind("rmi://localhost/rev", rev);
-            Naming.rebind("rmi://localhost/trie", tl);
-            Naming.rebind("rmi://localhost/produit", pr);
-            Naming.rebind("rmi://localhost/palindrome", pl);
+       //JNDI
+        	final Hashtable<Object, Object> jndiProperties = new Hashtable<Object, Object>();
+            jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
+            jndiProperties.put(Context.PROVIDER_URL, "rmi://localhost:1099");
             
-            System.out.println("Attente des invocations des clients !");
+            InitialContext ctx = new InitialContext(jndiProperties);
+            ctx.bind("palindrome", pl);
+        	
+        	System.out.println("Attente des clients !");
+        	
         } catch (Exception e) {
             System.out.println("Erreur de liaison de l'objet Reverse");
             System.out.println(e.toString());
